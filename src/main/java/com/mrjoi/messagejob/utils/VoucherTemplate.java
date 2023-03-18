b413d741e6618d66d8c493c1e80c4130f2270db4
JHONATAN JANSSEN SEGURA GALINDO <jsegurag@ripley.com.pe>
2023-03-18 18:06:47 -0500

    add(voucherTemplate.java): datos del cliente dinamico

--- src/main/java/com/mrjoi/messagejob/utils/VoucherTemplate.java
+++ src/main/java/com/mrjoi/messagejob/utils/VoucherTemplate.java
@@ -2,6 +2,10 @@ package com.mrjoi.messagejob.utils;
 
 import com.mrjoi.messagejob.model.BoletaEntrada;
 import com.mrjoi.messagejob.model.Reserva;
+import org.apache.commons.text.StringSubstitutor;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class VoucherTemplate {
 
@@ -10,7 +14,8 @@ public class VoucherTemplate {
     }
 
     public String getVoucherReservaTemplate(Reserva reserva) {
-        return """
+
+        String TEMPLATE = """
                 <body>
                     <div style="width:600px; margin: auto; font-family:Arial, Helvetica, sans-serif">
                         <div >
@@ -24,9 +29,10 @@ public class VoucherTemplate {
                         <hr style="min-width: 600px;height: 1px; margin: 1rem 0;color: inherit;background-color: currentColor;border: 0;">
                         <div  style="padding-bottom: 1em; display: flex;">
                             <div style="padding-right: 80px;">
-                                <h5 style="font-weight: 900;;font-size: 0.8em;">Cliente</h5>
-                                <p style="font-size: 15px;">Arian Manuel Garcia Reynoso</p>
-                                <p style="font-size: 15px;">Fecha Reserva: 26/02/2023 10:00 am</p>
+                                <h5 style="font-weight: 900;font-size: 0.8em;">Cliente</h5>
+                                <p style="font-size: 15px;">${nombres} ${apellidos}</p>
+                                <h5 style="font-weight: 900;font-size: 0.8em;">Fecha Reserva:</h5>
+                                <p style="font-size: 15px;">${fecha_reserva} ${hora_reserva} am</p>
                             </div>
                             <div style="padding-right: 40px;">
                                 <h5 style="font-weight: 900;;font-size: 0.8em;">N° de boleta:</h5>
@@ -34,14 +40,13 @@ public class VoucherTemplate {
                                 <h5 style="font-weight: 900;;font-size: 0.8em;">Fecha vencimiento:</h5>
                             </div>
                             <div>
-                                <h5 style="font-weight: 900;;font-size: 0.8em;">103</h5>
-                                <p>10/02/2023</p>
-                                <p>26/02/2023</p>
+                                <h5 style="font-weight: 900;;font-size: 0.8em;">${num_boleta}</h5>
+                                <p>${fecha_registro}</p>
+                                <p>${fecha_reserva}</p>
                             </div>
                         </div>
                         <div style="padding-bottom: 40px;display: grid;grid-template-rows: minmax(220px,auto);">
                             <table style="table-layout: fixed;border-collapse: collapse;">
-                       \s
                                 <thead style="border-top: solid 3px #000; border-bottom: 3px solid #000; height: 65px;">
                                 <tr>
                                     <th style="padding-top: 30px;">Cant. Personas</th>
@@ -52,10 +57,16 @@ public class VoucherTemplate {
                                 </thead>
                                 <tbody>
                                 <tr style="height:40px;">
-                                    <td style="text-align: center">1</td>
+                                    <td style="text-align: center">${cantidad_personas}</td>
                                     <td style="text-align: center">Reserva de cumpleaños</td>
-                                    <td style="text-align: right">0.00</td>
-                                    <td style="text-align: right">0.00</td>
+                                    <td style="text-align: right">${personas_price_unit}</td>
+                                    <td style="text-align: right">${personas_price_total}</td>
+                                </tr>
+                                <tr style="height:40px;">
+                                    <td style="text-align: center">${cantidad_acompaniante}</td>
+                                    <td style="text-align: center">Reserva de cumpleaños</td>
+                                    <td style="text-align: right">${acompaniante_price_unit}</td>
+                                    <td style="text-align: right">${acompaniante_price_total}</td>
                                 </tr>
                                 </tbody>
                                 <tfoot>
@@ -63,23 +74,23 @@ public class VoucherTemplate {
                                         <th></th>
                                         <th></th>
                                         <th style="text-align: right">V. venta</th>
-                                        <th style="text-align: right">0.00</th>
+                                        <th style="text-align: right">${valor_venta}</th>
                                     </tr>
                                     <tr style="height:30px;">
                                         <th></th>
                                         <th></th>
                                         <th style="text-align: right">IGV 18%</th>
-                                        <th style="text-align: right">0.00</th>
+                                        <th style="text-align: right">${IGV_total}</th>
                                     </tr>
                                     <tr style="height:30px;">
                                         <th></th>
                                         <th></th>
                                         <th style="text-align: right">Total </th>
-                                        <th style="text-align: right">0.00</th>
+                                        <th style="text-align: right">${tota_pago}</th>
                                     </tr>
                                 </tfoot>
                             </table>
-                        </div>\s
+                        </div>
                         <div  style="border-top: solid 2px #000; padding-top: 20px;">
                             <div style="display:flex">
                                 <div style="padding-right: 3em;">
@@ -100,6 +111,41 @@ public class VoucherTemplate {
                             </p>
                         </div>
                 </body>""";
+
+        double acompaniante_price_unit= 6.0;
+        double acompaniante_price_total= reserva.getAcompaniante() * acompaniante_price_unit ;
+        double personas_price_total = reserva.getTotalPago() - acompaniante_price_total;
+        double personas_price_unit = personas_price_total / reserva.getCantPersonas();
+        double IGV_perc = 0.18;
+        double IGV_total = reserva.getTotalPago() * 0.18;
+        double IGV_total_round = Math.round(IGV_total * 100.0) / 100.0;
+        double valor_venta = reserva.getTotalPago() - IGV_total;
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("nombres", reserva.getNombres());
+        params.put("apellidos", reserva.getApellido());
+        params.put("num_boleta", reserva.getIdReserva());
+
+        params.put("fecha_reserva", reserva.getFechaReserva());
+        params.put("hora_reserva", reserva.getHora());
+        params.put("fecha_registro", reserva.getFechaRegistro());
+        params.put("fecha_vencimiento", reserva.getFechaReserva());
+
+        params.put("cantidad_personas", reserva.getCantPersonas());
+        params.put("cantidad_acompaniante", reserva.getAcompaniante());
+
+        params.put("acompaniante_price_total", acompaniante_price_total);
+        params.put("acompaniante_price_unit", acompaniante_price_unit);
+        params.put("personas_price_total", personas_price_total);
+        params.put("personas_price_unit", personas_price_unit);
+
+        params.put("IGV_perc", IGV_perc);
+        params.put("IGV_total", IGV_total_round);
+        params.put("valor_venta", valor_venta);
+
+        params.put("tota_pago", reserva.getTotalPago());
+
+        return StringSubstitutor.replace(TEMPLATE, params, "${", "}");
     }
 
     public String getVoucherEntradaTemplate(BoletaEntrada ticket) {
@@ -117,24 +163,23 @@ public class VoucherTemplate {
                         <hr style="min-width: 600px;height: 1px; margin: 1rem 0;color: inherit;background-color: currentColor;border: 0;">
                         <div  style="padding-bottom: 1em; display: flex;">
                             <div style="padding-right: 80px;">
-                                <h5 style="font-weight: 900;;font-size: 0.8em;">Cliente</h5>
-                                <p style="font-size: 15px;">Arian Manuel Garcia Reynoso</p>
-                                <p style="font-size: 15px;">Fecha Reserva: 26/02/2023 10:00 am</p>
+                                <h5 style="font-weight: 900;font-size: 0.8em;">Cliente</h5>
+                                <p style="font-size: 15px;">${nombres}</p>
+                                <p style="font-size: 15px;">Fecha Reserva: ${fecha_reserva} ${hora_reserva} am</p>
                             </div>
                             <div style="padding-right: 40px;">
-                                <h5 style="font-weight: 900;;font-size: 0.8em;">N° de boleta:</h5>
-                                <h5 style="font-weight: 900;;font-size: 0.8em;">Fecha registro:</h5>
-                                <h5 style="font-weight: 900;;font-size: 0.8em;">Fecha vencimiento:</h5>
+                                <h5 style="font-weight: 900;font-size: 0.8em;">N° de boleta:</h5>
+                                <h5 style="font-weight: 900;font-size: 0.8em;">Fecha registro:</h5>
+                                <h5 style="font-weight: 900;font-size: 0.8em;">Fecha vencimiento:</h5>
                             </div>
                             <div>
                                 <h5 style="font-weight: 900;;font-size: 0.8em;">103</h5>
-                                <p>10/02/2023</p>
+                                <p>${fecha_registro}</p>
                                 <p>26/02/2023</p>
                             </div>
                         </div>
                         <div style="padding-bottom: 40px;display: grid;grid-template-rows: minmax(220px,auto);">
                             <table style="table-layout: fixed;border-collapse: collapse;">
-                       \s
                                 <thead style="border-top: solid 3px #000; border-bottom: 3px solid #000; height: 65px;">
                                 <tr>
                                     <th style="padding-top: 30px;">Cant. Personas</th>
@@ -172,7 +217,7 @@ public class VoucherTemplate {
                                     </tr>
                                 </tfoot>
                             </table>
-                        </div>\s
+                        </div>
                         <div  style="border-top: solid 2px #000; padding-top: 20px;">
                             <div style="display:flex">
                                 <div style="padding-right: 3em;">
